86efec65384d43f173a606865ff0f107bed1b376
Freddie <freddie.vinson.gwal@statefarm.com>
2018-09-26 08:57:27 -0600

    filestore

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.superbiz.moviefun.blobstore.BlobStore;
+import org.superbiz.moviefun.blobstore.FileStore;
 
 @SpringBootApplication
 public class Application {
@@ -16,4 +18,29 @@ public class Application {
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
+
+    @Bean
+    public BlobStore blobStore() {
+        return new FileStore();
+    }
+
+//    @Bean
+//    public BlobStore blobStore(
+//            ServiceCredentials serviceCredentials,
+//            @Value("${vcap.services.photo-storage.credentials.endpoint:#{null}}") String endpoint
+//    ) {
+//        String photoStorageAccessKeyId = serviceCredentials.getCredential("photo-storage", "user-provided", "access_key_id");
+//        String photoStorageSecretKey = serviceCredentials.getCredential("photo-storage", "user-provided", "secret_access_key");
+//        String photoStorageBucket = serviceCredentials.getCredential("photo-storage", "user-provided", "bucket");
+//
+//        AWSCredentials credentials = new BasicAWSCredentials(photoStorageAccessKeyId, photoStorageSecretKey);
+//        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+//
+//        if (endpoint != null) {
+//            s3Client.setEndpoint(endpoint);
+//        }
+//
+//        return new S3Store(s3Client, photoStorageBucket);
+//    }
+
 }
